2906ef1e74cdf9981d8714bc4a2f0f9e95c70981
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-03-28 22:25:20 +0530

    Update lastthirtyays-reg-users.php

--- loginsystem/admin/lastthirtyays-reg-users.php
+++ loginsystem/admin/lastthirtyays-reg-users.php
@@ -23,7 +23,7 @@ echo "<script>alert('Data deleted');</script>";
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Registered Users in Last 30 Days | Registration and Login System</title>
+        <title>admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="../css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
