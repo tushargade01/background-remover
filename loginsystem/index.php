87890118f9baba124dd04fc02a51279a24aeb684
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-04-07 18:34:04 +0530

    Update index.php

--- loginsystem/index.php
+++ loginsystem/index.php
@@ -1,9 +1,10 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BG Remover</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="css/questionSec.css">
@@ -16,7 +17,7 @@
 
 <body>
 <?php
-session_start(); 
+ 
     if(isset($_SESSION['id'])){
         include('includes/navuser.php');
     }else{
